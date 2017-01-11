8f7dc2626ebbd847a4c2cf2f5086b96dc53aa2e2
EvrenKayali <evren.kayali@bimsa.com>
2017-01-11 17:04:48 +0300

    appharbor conn str

--- MoneyBox.DAL/Migrations/Configuration.cs
+++ MoneyBox.DAL/Migrations/Configuration.cs
@@ -17,42 +17,42 @@
 
         protected override void Seed(MoneyBoxDb context)
         {
-            //string[] roles = new string[] { "Admin", "User", "CompanyOwner", "Employee" };
+            string[] roles = new string[] { "Admin", "User", "CompanyOwner", "Employee" };
 
-            //var store = new RoleStore<IdentityRole>(context);
-            //var manager = new RoleManager<IdentityRole>(store);
+            var store = new RoleStore<IdentityRole>(context);
+            var manager = new RoleManager<IdentityRole>(store);
 
 
-            //foreach (string role in roles)
-            //{
-            //    if (!context.Roles.Any(r => r.Name == role))
-            //    {
-            //        manager.Create(new IdentityRole(role));
-            //    }
-            //}
+            foreach (string role in roles)
+            {
+                if (!context.Roles.Any(r => r.Name == role))
+                {
+                    manager.Create(new IdentityRole(role));
+                }
+            }
 
-            //var cat1 = new Category { Name = "Yemek" };
-            //var cat2 = new Category { Name = "Giyim" };
+            var cat1 = new Category { Name = "Yemek" };
+            var cat2 = new Category { Name = "Giyim" };
 
-            //var subCat1 = new Category { Name = "Cafe & Restorant", ParentCategory = cat1 };
-            //var subCat2 = new Category { Name = "Ev Yemekleri", ParentCategory = cat1 };
-            //var subCat3 = new Category { Name = "Türk Mutfağı", ParentCategory = cat1 };
-            //var subCat4 = new Category { Name = "Fastfood", ParentCategory = cat1 };
+            var subCat1 = new Category { Name = "Cafe & Restorant", ParentCategory = cat1 };
+            var subCat2 = new Category { Name = "Ev Yemekleri", ParentCategory = cat1 };
+            var subCat3 = new Category { Name = "Türk Mutfağı", ParentCategory = cat1 };
+            var subCat4 = new Category { Name = "Fastfood", ParentCategory = cat1 };
 
-            //context.Categories.Add(subCat1);
-            //context.Categories.Add(subCat2);
-            //context.Categories.Add(subCat3);
-            //context.Categories.Add(subCat4);
+            context.Categories.Add(subCat1);
+            context.Categories.Add(subCat2);
+            context.Categories.Add(subCat3);
+            context.Categories.Add(subCat4);
 
-            //var subCat5 = new Category { Name = "İç Giyim", ParentCategory = cat2 };
-            //var subCat6 = new Category { Name = "Erkek Giyim", ParentCategory = cat2 };
-            //var subCat7 = new Category { Name = "Kadın Giyim", ParentCategory = cat2 };
-            //var subCat8 = new Category { Name = "Ayakkabı", ParentCategory = cat2 };
+            var subCat5 = new Category { Name = "İç Giyim", ParentCategory = cat2 };
+            var subCat6 = new Category { Name = "Erkek Giyim", ParentCategory = cat2 };
+            var subCat7 = new Category { Name = "Kadın Giyim", ParentCategory = cat2 };
+            var subCat8 = new Category { Name = "Ayakkabı", ParentCategory = cat2 };
 
-            //context.Categories.Add(subCat5);
-            //context.Categories.Add(subCat6);
-            //context.Categories.Add(subCat7);
-            //context.Categories.Add(subCat8);
+            context.Categories.Add(subCat5);
+            context.Categories.Add(subCat6);
+            context.Categories.Add(subCat7);
+            context.Categories.Add(subCat8);
 
 
         }
