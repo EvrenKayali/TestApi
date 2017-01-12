73d3d6cc56584ad3b1bc3fae9186bc188c76b0e3
EvrenKayali <evren.kayali@bimsa.com>
2017-01-12 15:37:44 +0300

    Custom exception handler added

---
+++ MoneyBox.WebApi/Filters/BusinessExceptionHandler.cs
@@ -0,0 +1,32 @@
+﻿using MoneyBox.Business.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http.Filters;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace MoneyBox.WebApi.Filters
+{
+    public class BusinessExceptionHandlerAttribute : ExceptionFilterAttribute
+    {
+        public override void OnException(HttpActionExecutedContext context)
+        {
+            if (context.Exception is BusinessException)
+            {
+                var request = context.ActionContext.Request;
+
+                var response = new
+                {
+                    Error = context.Exception.Message
+                };
+
+                context.Response = request.CreateResponse(HttpStatusCode.BadRequest, response);
+            }
+        }
+    }
+}
